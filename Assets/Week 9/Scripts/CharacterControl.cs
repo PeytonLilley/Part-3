bf39cd07d643cbf7b4a5b604a3f4e6488424bcd1
PeytonLilley <lilleye@sheridancollege.ca>
2024-03-13 12:48:47 -0400

    Task 4b in progress, text does not work yet

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    TextMeshProUGUI typeSelected;
+
+    private void Start()
+    {
+        typeSelected = GetComponent<TextMeshProUGUI>();
+        typeSelected.text = SelectedVillager.GetType().ToString();
+       
+    }
     public static void SetSelectedVillager(Villager villager)
     {
-        if(SelectedVillager != null)
+        if (SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
 }
