2dc35c7ea7f697207f7b1bc5866f46d062f7dc68
PeytonLilley <lilleye@sheridancollege.ca>
2024-03-30 18:27:35 -0400

    player checks what items it collides with using tags, inventory for each item increments when the player collects that item

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     bool walk;
     float moveX;
     float moveY;
+    public float keys = 0;
+    public float gems = 0;
+    public float hearts = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -38,5 +41,21 @@ public class Player : MonoBehaviour
         Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
         rb.AddForce(force);
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "key")
+        {
+            keys = keys + 1;
+        }
+        if (other.tag == "gem")
+        {
+            gems = gems + 1;
+        }
+        if (other.tag == "heart")
+        {
+            hearts = hearts + 1;
+        }
     }
 }
