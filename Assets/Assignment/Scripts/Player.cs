4ab537aeb67862e859ab3e6bd9e9ba69a0851f55
PeytonLilley <lilleye@sheridancollege.ca>
2024-03-29 23:00:06 -0400

    added walk animation for player

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
     public float speed = 500f;
+    bool walk;
+    float moveX;
+    float moveY;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        moveX = rb.velocity.x;
+        moveY = rb.velocity.y;
+        if (((moveX < 0.01) && (moveX > -0.01)) || ((moveY < 0.01) && (moveY > -0.01)))
+        {
+            animator.SetBool("walk", false);
+        }
+        if (((moveX > 0.01) || (moveX < -0.01)) || ((moveY > 0.01) || (moveY < -0.01)))
+        {
+           animator.SetBool("walk", true);
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
         rb.AddForce(force);
+        
     }
 }
